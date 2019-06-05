f25bf2ddb950d80fcc21a5d2b2dcdd46405f8fb2
Luis Benitez <luisbenitez02@gmail.com>
2019-06-05 17:30:40 -0500

    Documentos HTML agregados como estaticos
    Para evitar crear una ruta para cada uno y tener que reescribir parte del header del HTML se manejan todos como archivos estaticos HTML

--- src/controllers/misControladores.js
+++ src/controllers/misControladores.js
@@ -1,8 +1,26 @@
 /* Aqui agregaremos la funcionalidad general de la app */
 const controlador = {};
 
+//pagina de inicio o index
 controlador.inicio = (req, res) => {
      res.render('index');//pinto la vista 'customers'  
+}
+
+//---- Paginas estaticas
+controlador.rock = (req, res) => {
+     res.render('rock');//pinto la vista 'customers'  
+}
+
+controlador.pop = (req, res) => {
+     res.render('pop');//pinto la vista 'customers'  
+}
+
+controlador.swing = (req, res) => {
+     res.render('swing');//pinto la vista 'customers'  
+}
+
+controlador.jazz = (req, res) => {
+     res.render('jazz');//pinto la vista 'customers'  
 }
 
 module.exports = controlador;
--- src/routes/rutas.js
+++ src/routes/rutas.js
@@ -5,6 +5,12 @@ const router = express.Router();
 const misControladores = require('../controllers/misControladores');
 
 /* Lo que estoy escuchando */
-router.get('/', misControladores.inicio);//ejecuto el metodo list de customerController.js
+router.get('/', misControladores.inicio);
+
+/* Rutas estaticas  */
+router.get('/rock', misControladores.rock);
+router.get('/pop', misControladores.pop);
+router.get('/swing', misControladores.swing);
+router.get('/jazz', misControladores.jazz);
 
 module.exports = router;
