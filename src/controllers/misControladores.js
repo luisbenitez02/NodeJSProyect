a5ff94579b4c939510d7b826e5455050c762c289
Luis Benitez <luisbenitez02@gmail.com>
2019-06-05 21:48:31 -0500

    Actualizado buscar en una sola consulta JOIN
    Boton de buscar actualizado, todo se ejecuta en una sola consulta con JOIN y parametros LIKE OR

--- src/controllers/misControladores.js
+++ src/controllers/misControladores.js
@@ -9,14 +9,30 @@ controlador.inicio = (req, res) => {
 controlador.buscar = (req, res) => {
      const dato = req.query.dato;//obtengo dato que me entregaron URL
      //Solicito conexion a la BD
-     req.getConnection((err, conn) => {
+     const consulta = "SELECT art.name_artistas, art.link, ca.name_canciones, ca.letra FROM artistas art INNER JOIN canciones ca ON art.cod_artistas = ca.cod_artistas WHERE art.name_artistas " + " LIKE '%" + dato + "%'" + "OR ca.name_canciones" + " LIKE '%" + dato + "%'" + " OR ca.letra" + " LIKE '%" + dato + "%'";
 
-          conn.query("SELECT name_artistas, link FROM artistas WHERE name_artistas" + " LIKE '%" + dato + "%'",  (err, filas) => {
-               console.log(filas);
-               
-               res.render('resultado', { data: filas });
+     console.log(consulta);
+     req.getConnection((err, conn) => {
+          conn.query(consulta, (err, filas) => {
+               res.render('resultado', { data: filas });  
           });
+ 
+     /*req.getConnection((err, conn) => {
+          conn.query("SELECT name_artistas, link FROM artistas WHERE name_artistas" + " LIKE '%" + dato + "%'",  (err, filas) => {    
+               res.render('resultado', { data: filas });
+          });*/
+          
      });
 }
 
 module.exports = controlador;
+
+/* consultas */
+/*
+SELECT art.name_artistas, art.link, ca.name_canciones, ca.letra FROM artistas art INNER JOIN canciones ca ON art.cod_artistas = ca.cod_artistas WHERE LIKE art.cod_artistas = 1 
+
+----------------CORRECTA
+SELECT art.name_artistas, art.link, ca.name_canciones, ca.letra FROM artistas art INNER JOIN canciones ca ON art.cod_artistas = ca.cod_artistas WHERE art.name_artistas LIKE '%LOUIS%' OR ca.name_canciones LIKE '%LOUIS%' OR ca.letra LIKE '%LOUIS%'
+
+SELECT art.name_artistas, art.link, ca.name_canciones, ca.letra FROM artistas art INNER JOIN canciones ca ON art.cod_artistas = ca.cod_artistas WHERE art.name_artistas LIKE '%LOUIS%' 
+*/
